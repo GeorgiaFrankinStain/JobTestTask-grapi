c64d2d996bbe03bbbf66def89677d7d1f7ecb930
GeorgiaFrankinStain <georgiafrankinstain@gmail.com>
2023-08-07 06:02:19 +0700

    fix codestyle

--- javascript-nodejs/src/server.js
+++ javascript-nodejs/src/server.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 
-// var amqp = require('amqplib');
 var amqp = require('amqplib/callback_api');
 
 var express = require("express")
@@ -28,24 +27,6 @@ app.get("/api/users", async (req, res, next) => {
     const n = req.query.n;
 
     console.log(n);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     amqp.connect('amqp://localhost', function(error0, connection) {
         if (error0) {
             throw error0;
@@ -73,10 +54,6 @@ app.get("/api/users", async (req, res, next) => {
                         console.log(' [.] Got %s', msg.content.toString());
 
                         res.status(200).json({"fib":msg.content.toString()});
-                        // setTimeout(function() {
-                        //     connection.close();
-                        //     process.exit(0);
-                        // }, 500);
                     }
                 }, {
                     noAck: true
@@ -90,115 +67,7 @@ app.get("/api/users", async (req, res, next) => {
                     });
             });
         });
-
-
-
     });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-
-
-
-
-
-
-    let connection;
-    try {
-        connection = await amqp.connect("amqp://localhost");
-
-
-        var correlationId = generateUuid();
-        var num = parseInt(n);
-
-
-        const channel = await connection.createChannel();
-
-        const q = await channel.assertQueue('rpc_queue', { durable: false });
-        channel.sendToQueue('rpc_queue',
-            // Buffer.from("4"), {
-            Buffer.from(num.toString()), {
-                correlationId: correlationId,
-                replyTo: q.queue
-            });
-        // console.log(" [x] Sent '%s'", text);
-
-
-
-        channel.prefetch(1, false); // global=false
-        await channel.consume(q.queue, async function(msg) {
-            if (msg.properties.correlationId === correlationId) {
-                console.log(' [.] Got %s', msg.content.toString());
-
-                // setTimeout(function() {
-                //     connection.close();
-                //     process.exit(0);
-            }
-
-
-
-            // }, 500);
-            res.status(200).json({"error":msg.content.toString()});
-            // return;
-            channel.ack(message);
-        }, {
-            noAck: true
-        });
-
-
-        await channel.close();
-    } catch (err) {
-        console.warn(err);
-    } finally {
-        if (connection) await connection.close();
-    }
-
-
-
-*/
-
-
-
-
-
-
-
-
-    if (true) {
-        // res.status(400).json({"error":"err.message"});
-        return;
-    } else {
-        res.json({
-            "message":"success",
-            "data":"rows"
-        })
-    }
 });
 
 
