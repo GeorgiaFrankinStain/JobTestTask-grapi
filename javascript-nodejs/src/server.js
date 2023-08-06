dd074c354914d28a7725d164862ab5fa1bc59f8f
GeorgiaFrankinStain <georgiafrankinstain@gmail.com>
2023-08-07 03:57:58 +0700

    add transport argument from http get to M2

--- javascript-nodejs/src/server.js
+++ javascript-nodejs/src/server.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+
+var amqp = require('amqplib');
+
 var express = require("express")
 var app = express()
 
@@ -14,11 +17,72 @@ app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 
-app.get("/api/users", (req, res, next) => {
+app.get("/api/users", async (req, res, next) => {
 
     const n = req.query.n;
 
     console.log(n);
+
+
+
+
+
+
+
+
+
+    let connection;
+    try {
+        connection = await amqp.connect("amqp://localhost");
+
+
+        var correlationId = generateUuid();
+        var num = parseInt(n);
+
+
+        const channel = await connection.createChannel();
+
+        const q = await channel.assertQueue('rpc_queue', { durable: false });
+        channel.sendToQueue('rpc_queue',
+            // Buffer.from("4"), {
+            Buffer.from(num.toString()), {
+                correlationId: correlationId,
+                replyTo: q.queue
+            });
+        // console.log(" [x] Sent '%s'", text);
+
+
+
+
+        // await channel.consume(q.queue, function(msg) {
+        //     if (msg.properties.correlationId === correlationId) {
+        //         console.log(' [.] Got %s', msg.content.toString());
+        //         setTimeout(function() {
+        //             connection.close();
+        //             process.exit(0);
+        //         }, 500);
+        //     }
+        // }, {
+        //     noAck: true
+        // });
+
+
+        await channel.close();
+    } catch (err) {
+        console.warn(err);
+    } finally {
+        if (connection) await connection.close();
+    }
+
+
+
+
+
+
+
+
+
+
 
     if (true) {
         res.status(400).json({"error":"err.message"});
@@ -37,3 +101,11 @@ app.get("/api/users", (req, res, next) => {
 app.get("/", (req, res, next) => {
     res.json({"message":"Ok"})
 });
+
+
+
+function generateUuid() {
+    return Math.random().toString() +
+        Math.random().toString() +
+        Math.random().toString();
+}
