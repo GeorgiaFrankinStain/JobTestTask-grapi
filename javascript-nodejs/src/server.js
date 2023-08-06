c41764773396fe781389be688e52f8da15038edb
GeorgiaFrankinStain <georgiafrankinstain@gmail.com>
2023-08-07 00:51:28 +0700

    add listen port

---
+++ javascript-nodejs/src/server.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+var express = require("express")
+var app = express()
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var HTTP_PORT = 8000
+
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+
+    if (true) {
+        res.status(400).json({"error":"err.message"});
+        return;
+    } else {
+        res.json({
+            "message":"success",
+            "data":"rows"
+        })
+    }
+});
+
+
+
+// Root path
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
